aee5a6d025020ec239670979089ca195c7775cce
Devapandian <devaazhagu@gmail.com>
2023-09-28 14:34:19 +0530

    change

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,69 +1,80 @@
-import React, { useEffect , useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Button from "react-bootstrap/Button";
 import { UserAuth } from "../context/AuthContext";
 import axios from "axios";
 import { toast } from "react-toastify";
 import TextField from "@mui/material/TextField";
-import {  getAuth, GoogleAuthProvider, signInWithPopup,signInWithRedirect } from "firebase/auth"; // Import the required Firebase authentication functions
-
-
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  FacebookAuthProvider,
+  GithubAuthProvider,
+} from "firebase/auth";
 
 const Signin = () => {
   const { googleSignIn, githubSignIn } = UserAuth();
   const navigate = useNavigate();
 
   const [user, setUser] = useState(null);
-  // const handleGoogleSignIn = async (e) => {
-  //   e.preventDefault();
-  //   const auth = getAuth();
-  //   const provider = new GoogleAuthProvider();
-  //   try {
-  //     await signInWithRedirect(auth, provider);
-  //           navigate("/Dashboard");
-
-  //   } catch (error) {
-  //     console.log("Google Sign-In Error:", error);
-  //   }
-  // };
-  const [value,setValue] = useState('')
-
+  const [value, setValue] = useState("");
 
   const handleGoogleSignIn = async (e) => {
     const auth = getAuth();
-    const provider = new GoogleAuthProvider(); 
+    const googleProvider = new GoogleAuthProvider();
     try {
-      const data = await signInWithPopup(auth, provider);
-      setValue(data.user.email)
-      localStorage.setItem("email",data.user.email)
-      setUser(data.user); // Set the user state to the signed-in user
+      const data = await signInWithPopup(auth, googleProvider);
+      setValue(data.user.email);
+      localStorage.setItem("email", data.user.email);
+      setUser(data.user.email);
       console.log(data.user);
 
       navigate("/Dashboard");
-
     } catch (error) {
       console.log("Google Sign-In Error:", error);
     }
-  }
+  };
   console.log(user);
 
-useEffect(()=>{
-}, [user]);
-console.log(user);
+  useEffect(() => {}, [user]);
+  console.log(user);
 
-  const handleGithubSignIn = async (e) => {
-    let result;
+  const handleFacebookSignIn = async (e) => {
+    const auth = getAuth();
+    const facebookProvider = new FacebookAuthProvider();
     try {
-      result = await githubSignIn();
-      console.log(result, user);
-      if (user) {
-        sessionStorage.clear();
-        navigate("/Dashboard");
-      }
+      const data = await signInWithPopup(auth, facebookProvider);
+      setValue(data.user.email);
+      localStorage.setItem("email", data.user.email);
+      setUser(data.user.email);
+      console.log(data.user);
+
+      navigate("/Dashboard");
     } catch (error) {
-      console.log(error);
+      console.log("Google Sign-In Error:", error);
     }
   };
+  console.log(user);
+
+  useEffect(() => {}, [user]);
+  console.log(user);
+  const handleGithubSignIn = async (e) => {
+    const auth = getAuth();
+    const githubProvider = new GithubAuthProvider();
+    try {
+      const data = await signInWithPopup(auth, githubProvider);
+      setValue(data.user.email);
+      localStorage.setItem("email", data.user.email);
+      setUser(data.user.email);
+      console.log(data.user);
+
+      navigate("/Dashboard");
+    } catch (error) {
+      console.log("Google Sign-In Error:", error);
+    }
+  };
+  console.log(user);
   console.log(user);
   const handleSignin = async (e) => {
     e.preventDefault();
@@ -91,7 +102,8 @@ console.log(user);
   return (
     <div
       style={{
-        backgroundImage: "url('https://images.alphacoders.com/995/995566.jpg')",
+        backgroundImage:
+          "url('https://img.freepik.com/premium-photo/dark-background-with-red-leaves-circle-red-leaves_421632-16209.jpg')",
         backgroundSize: "cover",
         minHeight: "100vh",
       }}
@@ -102,6 +114,7 @@ console.log(user);
           fontSize: "24px",
           fontWeight: "bold",
           padding: "20px",
+          color: "pink",
         }}
       >
         SIGN IN
@@ -113,32 +126,47 @@ console.log(user);
             type="email"
             placeholder="Enter email"
             name="email"
-            variant="outlined"
+            variant="standard"
             margin="normal"
             fullWidth
+            InputLabelProps={{
+              style: {
+                color: "white",
+              },
+            }}
           />
           <TextField
             label="Password"
             type="password"
             placeholder="Password"
             name="password"
-            variant="outlined"
+            variant="standard"
             margin="normal"
+            style={{ borderbottom: "2px solid white" }}
             fullWidth
-          />
-          <Button
-            variant="contained"
-            color="primary"
-            type="submit"
-            fullWidth
-            style={{
-              marginTop: "18px",
-              width: "100%",
-              backgroundColor: "pink",
+            InputLabelProps={{
+              style: {
+                color: "white",
+              },
             }}
-          >
-            Submit
-          </Button>
+          />
+          <div style={{ textAlign: "center" }}>
+            <Button
+              variant="contained"
+              color="primary"
+              type="submit"
+              fullWidth
+              style={{
+                marginTop: "18px",
+                width: "30%",
+                backgroundColor: "green",
+                color: "white",
+              }}
+            >
+              Submit
+            </Button>
+          </div>
+          &nbsp;
         </form>
         <Button
           variant="contained"
@@ -146,45 +174,65 @@ console.log(user);
           onClick={handleGoogleSignIn}
           style={{
             marginTop: "16px",
-            backgroundColor: "blue",
             color: "white",
-            width: "100%",
+            width: "30%",
           }}
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="35"
             height="20"
-            fill="currentColor"
+            fill="green"
             class="bi bi-google"
             viewBox="0 0 16 16"
           >
             <path d="M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z" />
-          </svg>{" "}
-          Google Signin
+          </svg>
+          Google
         </Button>
         <Button
           variant="contained"
           type="submit"
-          onClick={handleGoogleSignIn}
+          onClick={handleGithubSignIn}
           style={{
             marginTop: "16px",
-            backgroundColor: "blue",
             color: "white",
-            width: "100%",
+            width: "30%",
           }}
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="35"
             height="20"
-            fill="currentColor"
+            fill="green"
             class="bi bi-github"
             viewBox="0 0 16 16"
           >
             <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
-          </svg>{" "}
-          Git hub
+          </svg>
+          Github
+        </Button>
+        <Button
+          variant="contained"
+          type="submit"
+          onClick={handleFacebookSignIn}
+          style={{
+            marginTop: "16px",
+            color: "white",
+            width: "35%",
+          }}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="35"
+            height="20"
+            fill="green"
+            class="bi bi-facebook"
+            viewBox="0 0 16 16"
+          >
+            <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z" />
+          </svg>
+          Facebook
         </Button>
       </div>
     </div>
--- src/components/firebase.js
+++ src/components/firebase.js
@@ -1,18 +1,21 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider ,signInWithPopup } from "firebase/auth";
+import { getAuth, GoogleAuthProvider ,FacebookAuthProvider,GithubAuthProvider,signInWithPopup } from "firebase/auth";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCmrQbquEWu6AmTdwx_Hune8DAUS8HXjAs",
-  authDomain: "login-in-85856.firebaseapp.com",
-  projectId: "login-in-85856",
-  storageBucket: "login-in-85856.appspot.com",
-  messagingSenderId: "96659129698",
-  appId: "1:96659129698:web:012f79f37a7a0ae61c983b"
+  apiKey: "AIzaSyAKz1Fdf8y8QEv_N5vUaX9RWlfkX53iWaA",
+  authDomain: "fir-3f53e.firebaseapp.com",
+  projectId: "fir-3f53e",
+  storageBucket: "fir-3f53e.appspot.com",
+  messagingSenderId: "246231238781",
+  appId: "1:246231238781:web:5dc51fcd94bf19e2494914"
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-const provider = new GoogleAuthProvider();
+const googleProvider = new GoogleAuthProvider();
+const githubProvider = new GithubAuthProvider();
+const facebookProvider = new FacebookAuthProvider();
 
-export {auth, provider}
+
+export {auth, googleProvider,githubProvider,facebookProvider}

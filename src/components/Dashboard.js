c08ffc7103a31613c9ec3357942293d6e65b953f
Devapandian <devaazhagu@gmail.com>
2023-10-11 17:23:40 +0530

    change

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -68,7 +68,7 @@ function Dashboard() {
         <div className="row">
           {cardData.map((card, index) => (
             <div key={index} className="col-12 col-md-4 mb-4">
-              <Card style={{ boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.2)',marginTop:"20px",width:"350px" }}>
+              <Card style={{ boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.2)',marginTop:"20px",width:"350px"}}>
                 <Card.Body className="d-flex flex-column">
                   <Card.Img variant="top" src={card.imageUrl} style={{ height: '180px' }} />
                   <Card.Title className={classes.Card}>{card.title}</Card.Title>
